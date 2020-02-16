0a48586ceff2219f286f25cc8538964f0415692e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-16 19:49:28 +0100

    Interface definition

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([100000, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([9999, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
